e0fc6e448ce6208f735f2209200185ebbea90bde
Friederik <70342668+Friederik@users.noreply.github.com>
2025-06-19 17:40:19 +0500

    Understanding API

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,4 +1,19 @@
-fetch('http://api.weatherapi.com/v1/forecast.json?key=7b7ba9234b984c6eba9115539251906&q=Ekaterinburg&days=1&aqi=no&alerts=no')
+const BASEURL = 'http://api.weatherapi.com/v1'
+const APIKEY = '7b7ba9234b984c6eba9115539251906'
+
+function request() {
+    const requestData = {
+        type: 'forecast',
+    }    
+    return function getData() {
+        return `${BASEURL}/${requestData.type}.json?key=${APIKEY}&q=Ekaterinburg&days=1&aqi=no&alerts=no`
+    }
+}
+
+const requestConfig = request()
+
+
+fetch(requestConfig())
     .then((res) => {
         if (!res.ok) throw new Error('Ошбика загрузки')
         return res.json()
